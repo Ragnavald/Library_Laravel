af468c24c879d5920242d0ef7970d41ee941e3b5
ROnaldo <ronaldoavilajunior@outlook.com>
2024-01-26 14:07:42 -0300

    adding icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/app.css">
+    <link rel="icon" href="/images/logo.png">
     <link rel='stylesheet' type='text/css' media='screen' href="{{asset('/css/app.css')}}">
     <script src='main.js'></script>
     <title>{{$title}}</title>
